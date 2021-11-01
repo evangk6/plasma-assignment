55042a9ad0d24c165b0b9375724e42d8102f54a4
evangk6 <36457549+evangk6@users.noreply.github.com>
2021-10-31 21:37:14 -0400

    Feature history (#3)
    * create queries
    
    * show histories
    
    * add history info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { ToolbarComponent } from './components/layout/toolbar/toolbar.component';
+import { HistoriesComponent } from './components/histories/histories.component';
+import { HistoryComponent } from './components/history/history.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    HistoriesComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
